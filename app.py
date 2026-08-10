1a167a7668a07bc45433605ce3e7d91e8a1e97ab
moovwebrender <moov.web.render@gmail.com>
2026-08-10 00:16:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -331,10 +331,10 @@ async def recharge():
 
 
 
-    if int(user.get("total_money", 0)) >= 6000:
+    if int(user.get("total_money", 0)) >= 5500:
         return jsonify({
         "status": "error",
-        "message": "لقد تجاوزت الحد الأقصى المسموح به (6000). يرجى تسديد مستحقاتك للمدير أولاً."
+        "message": "لقد تجاوزت الحد الأقصى المسموح به (5500). يرجى تسديد مستحقاتك للمدير أولاً."
     }), 400
 
 
